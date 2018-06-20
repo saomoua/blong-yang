5ff9350d7dd102eb05d44663f57714ba505cb885
Sao Moua <sao.moua@Moua-Family.local>
2018-06-19 23:55:57 -0500

    master: tweak gulp watch tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,17 @@
 'use strict';
 
-import plugins       from 'gulp-load-plugins';
-import yargs         from 'yargs';
-import browser       from 'browser-sync';
-import gulp          from 'gulp';
-import panini        from 'panini';
-import rimraf        from 'rimraf';
-import sherpa        from 'style-sherpa';
-import yaml          from 'js-yaml';
-import fs            from 'fs';
+import plugins from 'gulp-load-plugins';
+import yargs from 'yargs';
+import browser from 'browser-sync';
+import gulp from 'gulp';
+import panini from 'panini';
+import rimraf from 'rimraf';
+import sherpa from 'style-sherpa';
+import yaml from 'js-yaml';
+import fs from 'fs';
 import webpackStream from 'webpack-stream';
-import webpack2      from 'webpack';
-import named         from 'vinyl-named';
+import webpack2 from 'webpack';
+import named from 'vinyl-named';
 
 // Load all Gulp plugins into one variable
 const $ = plugins();
@@ -20,7 +20,12 @@ const $ = plugins();
 const PRODUCTION = !!(yargs.argv.production);
 
 // Load settings from settings.yml
-const { COMPATIBILITY, PORT, UNCSS_OPTIONS, PATHS } = loadConfig();
+const {
+  COMPATIBILITY,
+  PORT,
+  UNCSS_OPTIONS,
+  PATHS
+} = loadConfig();
 
 function loadConfig() {
   let ymlFile = fs.readFileSync('config.yml', 'utf8');
@@ -29,7 +34,7 @@ function loadConfig() {
 
 // Build the "dist" folder by running all of the below tasks
 gulp.task('build',
- gulp.series(clean, gulp.parallel(svgSprite), pages, sass, images, injectSvgSprite, javascript, copy, styleGuide));
+  gulp.series(clean, gulp.parallel(svgSprite), pages, sass, images, injectSvgSprite, javascript, copy, styleGuide));
 
 // Build the site, run the server, and watch for file changes
 gulp.task('default',
@@ -81,32 +86,32 @@ function sass() {
   return gulp.src(PATHS.srcScssApp)
     .pipe($.sourcemaps.init())
     .pipe($.sass({
-      includePaths: PATHS.sassLibraries
-    })
+        includePaths: PATHS.sassLibraries
+      })
       .on('error', $.sass.logError))
     .pipe($.autoprefixer({
       browsers: COMPATIBILITY
     }))
     // Comment in the pipe below to run UnCSS in production
     //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
-    .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie9' })))
+    .pipe($.if(PRODUCTION, $.cleanCss({
+      compatibility: 'ie9'
+    })))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
     .pipe(gulp.dest(PATHS.distCSS))
-    .pipe(browser.reload({ stream: true }));
+    .pipe(browser.reload({
+      stream: true
+    }));
 }
 
 let webpackConfig = {
   module: {
-    rules: [
-      {
-        test: /.js$/,
-        use: [
-          {
-            loader: 'babel-loader'
-          }
-        ]
-      }
-    ]
+    rules: [{
+      test: /.js$/,
+      use: [{
+        loader: 'babel-loader'
+      }]
+    }]
   }
 }
 // Combine JavaScript into one file
@@ -117,7 +122,9 @@ function javascript() {
     .pipe($.sourcemaps.init())
     .pipe(webpackStream(webpackConfig, webpack2))
     .pipe($.if(PRODUCTION, $.uglify()
-      .on('error', e => { console.log(e); })
+      .on('error', e => {
+        console.log(e);
+      })
     ))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
     .pipe(gulp.dest(PATHS.distJS));
@@ -166,7 +173,8 @@ function injectSvgSprite() {
 // Start a server with BrowserSync to preview the site in
 function server(done) {
   browser.init({
-    server: PATHS.dist, port: PORT
+    server: PATHS.dist,
+    port: PORT
   });
   done();
 }
@@ -182,7 +190,7 @@ function watch() {
   gulp.watch(PATHS.assets, copy);
   gulp.watch(PATHS.srcPages).on('all', gulp.series(pages, browser.reload));
   gulp.watch(PATHS.srcPagesData).on('all', gulp.series(resetPages, pages, browser.reload));
-  gulp.watch(PATHS.srcFilesScss).on('all', sass);
+  gulp.watch(PATHS.srcFilesScss).on('all', gulp.series(sass, browser.reload));
   gulp.watch(PATHS.srcFilesJS).on('all', gulp.series(javascript, browser.reload));
   gulp.watch(PATHS.srcFilesImages).on('all', gulp.series(images, browser.reload));
   gulp.watch(PATHS.srcFilesSVG).on('all', gulp.series(svgSprite, injectSvgSprite, browser.reload));
