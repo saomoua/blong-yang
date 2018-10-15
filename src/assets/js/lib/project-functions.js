26208ef21b186307b018c8c8dc2253ddf4f51fb6
Sao Moua <sao.moua@Moua-Family.local>
2018-10-14 22:36:52 -0500

    dev: consolidate .menu-lvl-1 styles

--- src/assets/js/lib/project-functions.js
+++ src/assets/js/lib/project-functions.js
@@ -4,6 +4,13 @@ var mainContents = $(".main-contents"),
   siteHeader = $(".site-header"),
   siteHeaderHeight = $(".site-header").outerHeight();
 
+function siteNav() {
+  $('.menu-label').on("click", function(e) {
+    e.preventDefault();
+    $(this).next('.menu-lvl-2').addClass('menu-active').fadeIn();
+  });
+}
+
 function pushMainContents() {
   mainContents.css({
     "margin-top": siteHeaderHeight
@@ -11,6 +18,9 @@ function pushMainContents() {
 }
 
 pushMainContents();
+siteNav();
+
 $(window).on("resize", function() {
   pushMainContents();
+  siteNav();
 });
