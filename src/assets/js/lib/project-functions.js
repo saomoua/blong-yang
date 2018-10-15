eaffbc6d99f1b0fd5e3d7d28e2e337caf2814f6c
Sao Moua <sao.moua@Moua-Family.local>
2018-10-15 00:11:23 -0500

    dev: add click function to close mobile menu when clicking outside menu

--- src/assets/js/lib/project-functions.js
+++ src/assets/js/lib/project-functions.js
@@ -5,7 +5,7 @@ var mainContents = $(".main-contents"),
   siteHeaderHeight = $(".site-header").outerHeight();
 
 function siteNav() {
-  $('.menu-label').unbind().on("click", function(e) {
+  $('.menu-label').unbind().on("click", function (e) {
     e.preventDefault();
     $(this).find('.svg').toggleClass('icon-rotated');
     $(this).next('.menu-lvl-2').toggleClass('menu-active').fadeToggle();
@@ -21,7 +21,18 @@ function pushMainContents() {
 pushMainContents();
 siteNav();
 
-$(window).on("resize", function() {
+$(window).on("resize", function () {
   pushMainContents();
   siteNav();
+});
+
+$('html').click(function () {
+  if ($(window).width() >= 1024) {
+    // close menu when clicking anywhere outside on large screens
+    // $('li.has-children').find('.svg').removeClass('icon-rotated').find('.menu-lvl-2').removeClass('menu-active').fadeOut();
+  } else {
+    // close menu when clicking anywhere outside on small screens
+    $('.site-nav-offcanvas').removeClass('is-open');
+    $('.js-off-canvas-overlay').removeClass('is-visible');
+  }
 });
